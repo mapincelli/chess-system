1508e715a480116e86804051038ddd76b42a0be1
Marcelo Pincelli <marcelopincellit@hotmail.com>
2024-06-24 09:55:26 -0300

    New packages and Class Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,29 @@
+package boardgame;
+
+public class Position {
+    private Integer row;
+    private Integer column;
+
+    public Position (Integer row, Integer column){
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow(){
+        return row;
+    }
+    public void setRow(int row){
+        this.row = row;
+    }
+    public int getColumn(){
+        return column;
+    }
+    public void setColumn(int column){
+        this.column = column;
+    }
+
+    @Override
+    public String toString(){
+        return row + "," + column;
+    }
+}
