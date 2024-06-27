841de435a848c0d03524491dc1a23acc6c035758
Marcelo Pincelli <marcelopincellit@hotmail.com>
2024-06-27 09:08:13 -0300

    Piece simple move

--- src/program/Main.java
+++ src/program/Main.java
@@ -2,10 +2,26 @@ package program;
 
 import boardgame.Board;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         ChessMatch chessMatch = new ChessMatch();
-        application.UI.printBoard(chessMatch.getPieces());
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            application.UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source:");
+            ChessPosition source = application.UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = application.UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source,target);
+        }
     }
 }
