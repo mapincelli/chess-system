dd2e7d1e63e295453b59076dba65ef8b7a714e30
Marcelo Pincelli <marcelopincellit@hotmail.com>
2024-06-27 08:51:26 -0300

    Piece color change based on StackOverFlow

--- src/program/Main.java
+++ src/program/Main.java
@@ -6,6 +6,6 @@ import chess.ChessMatch;
 public class Main {
     public static void main(String[] args) {
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+        application.UI.printBoard(chessMatch.getPieces());
     }
 }
