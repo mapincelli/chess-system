0284016ccff003a66bc105dbf5ffb5bafdabce2e
Marcelo Pincelli <marcelopincellit@hotmail.com>
2024-06-27 10:45:31 -0300

    Exceptions e clear screen

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,10 +1,12 @@
 package program;
 
 import boardgame.Board;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -12,16 +14,27 @@ public class Main {
         ChessMatch chessMatch = new ChessMatch();
         Scanner sc = new Scanner(System.in);
         while (true) {
-            application.UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source:");
-            ChessPosition source = application.UI.readChessPosition(sc);
+            try {
+                application.UI.clearScreen();
+                application.UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source:");
+                ChessPosition source = application.UI.readChessPosition(sc);
 
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition target = application.UI.readChessPosition(sc);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = application.UI.readChessPosition(sc);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source,target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            }
+            catch (ChessException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
     }
 }
--- src/program/UI.java
+++ src/program/UI.java
@@ -44,27 +44,29 @@ public class UI {
     private static void printPiece(ChessPiece piece) {
         if (piece == null) {
             System.out.print("-");
-        }
-        else {
+        } else {
             if (piece.getColor() == Color.WHITE) {
                 System.out.print(ANSI_WHITE + piece + ANSI_RESET);
-            }
-            else {
+            } else {
                 System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
             }
         }
         System.out.print(" ");
     }
 
-    public static ChessPosition readChessPosition(Scanner sc){
+    public static ChessPosition readChessPosition(Scanner sc) {
         try {
             String s = sc.nextLine();
             char column = s.charAt(0);
             int row = Integer.parseInt(s.substring(1));
             return new ChessPosition(column, row);
-        }
-        catch (RuntimeException e){
+        } catch (RuntimeException e) {
             throw new InputMismatchException("Error reading ChessPosition.");
         }
+    }
+
+    public static void clearScreen () {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
