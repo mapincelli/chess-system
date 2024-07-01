1895992c05da5155013e3ce3b60842752c060dd5
Marcelo Pincelli <marcelopincellit@hotmail.com>
2024-07-01 17:06:15 -0300

    Horse Class

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -3,10 +3,7 @@ package chess;
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
-import chess.pieces.Bishop;
-import chess.pieces.King;
-import chess.pieces.Pawn;
-import chess.pieces.Tower;
+import chess.pieces.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -208,6 +205,8 @@ public class ChessMatch {
         placeNewPiece('h' ,2 , new Pawn(board,Color.WHITE));
         placeNewPiece('c', 1 , new Bishop(board, Color.WHITE));
         placeNewPiece('f', 1 , new Bishop(board, Color.WHITE));
+        placeNewPiece('b', 1, new Horse(board, Color.WHITE));
+        placeNewPiece('g',1, new Horse(board, Color.WHITE));
 
         //BLACK side
         placeNewPiece('a' ,8 , new Tower(board,Color.BLACK));
@@ -223,5 +222,7 @@ public class ChessMatch {
         placeNewPiece('h' ,7 , new Pawn(board,Color.BLACK));
         placeNewPiece('f', 8 , new Bishop(board, Color.BLACK));
         placeNewPiece('c', 8 , new Bishop(board, Color.BLACK));
+        placeNewPiece('b', 8 , new Horse(board, Color.BLACK));
+        placeNewPiece('g', 8, new Horse(board, Color.BLACK));
     }
 }
